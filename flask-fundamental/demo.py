cb6ccf5ddd3e044cde5a7a088f79b6e8553831b7
isaac-adedoyin <aderonmuisaac@gmail.com>
2020-10-08 04:42:16 +0100

    feat: add /stuff route

--- flask-fundamental/demo.py
+++ flask-fundamental/demo.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -8,6 +8,13 @@ def home():
     Function description goes here
     """
     return render_template("index.html")
+
+@app.route("/stuff")
+def stuff():
+    """
+    docstring
+    """
+    return render_template("stuff.html")
 
 if __name__ == "__main__":
     app.run(port=7000, debug=True)
