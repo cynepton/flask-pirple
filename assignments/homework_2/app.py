59fe571f8f39abdaad0cec2022363980e0685445
isaac-adedoyin <aderonmuisaac@gmail.com>
2020-10-09 23:30:54 +0100

    feat: homework_2

--- assignments/homework_2/app.py
+++ assignments/homework_2/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/about", methods=["GET"])
+def home():
+    """
+    Function description goes here
+    """
+    return render_template("about_us.html", title="About Us")
 
 if __name__ == "__main__":
     app.run(debug=True)
